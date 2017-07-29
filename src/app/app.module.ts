780b822f1a8fc39898000717b2a59d4be4328736
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-07-29 17:44:14 +0100

    creates file structure and initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe_book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe_book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe_book/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping_list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping_list/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
